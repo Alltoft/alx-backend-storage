4bc1564af7a15a2e1869fb0910df6ebcd0febce1
Emran <ouakouriimran@gmail.com>
2024-06-12 17:32:42 +0100

    0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
----creats a table
+-- We are all unique!
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREAMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
 );
